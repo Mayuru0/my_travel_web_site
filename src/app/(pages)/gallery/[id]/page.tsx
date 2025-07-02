6177ee6c7c744ea0dac01f684023625ea97c0745
Mayuru Madhuranga <mayurumaduranga@gmail.com>
2025-07-02 23:22:44 +0530

    bug fix

--- src/app/(pages)/gallery/[id]/page.tsx
+++ src/app/(pages)/gallery/[id]/page.tsx
@@ -1,16 +1,19 @@
 "use client";
-import React from 'react'
+
+import React from "react";
 import { useParams } from "next/navigation";
-import Gallery from '@/components/Pages/Gallery/Gallery';
-import GalleryHero from '@/components/Pages/Gallery/GalleryHero';
-const page = () => {
-    const { id } = useParams<{ id: string }>();
+import Gallery from "@/components/Pages/Gallery/Gallery";
+import GalleryHero from "@/components/Pages/Gallery/GalleryHero";
+
+const Page = () => {
+  const { id } = useParams<{ id: string }>();
+
   return (
     <div>
       <GalleryHero />
       <Gallery galleryId={id} />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
